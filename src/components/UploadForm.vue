181ed72461b6346175c76685e4f017dbbec1d0ba
Denis Blazhkun <dnzg666@gmail.com>
2018-08-16 18:21:48 +0300

    init repo

---
+++ src/components/UploadForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="dropper">
+    <input
+      @change="uploadImages($event.target.files)"
+      type="file"
+      multiple
+      accept="image/*"
+    />
+    <span>Drag files here!</span>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'UploadForm',
+  methods: mapActions(['uploadImages'])
+}
+</script>
+
+<style scoped>
+.dropper {
+  height: 30vh;
+  border: 2px dashed black;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  transition: all ease-in 0.1s;
+}
+
+.dropper:hover {
+  background-color: #eee;
+  transition: all ease-in 0.1s;
+}
+
+input {
+  width: 100%;
+  height: 30vh;
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+}
+
+span {
+  font-size: 20px;
+}
+</style>
